ba42959288139377c23fdc500e13f772027c956c
Dana <martinez.dbethel@gmail.com>
2022-06-12 17:38:38 -0400

    Add files via upload

---
+++ Chapter 4/dimensions.py
@@ -0,0 +1,13 @@
+dimensions = (200, 50)
+#dimensions[0] = 250
+print("Original dimension:")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400,100)
+print ("\nModified dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+print(dimensions[0])
+print(dimensions[1])
---
+++ Chapter 4/foods.py
@@ -0,0 +1,14 @@
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+
+for top_food in my_foods:
+    print(top_food.title() + " is delicious!")
+    
+print("My favorite foods are:")
+print(my_foods)
+
+print("\nMy friend's favorite foods are:")
+print(friend_foods)
